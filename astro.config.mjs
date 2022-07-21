9c103284a42d84d9e6566f17fcbb2a8c6ca5afdf
mesinkasir <hockeycorpmarketing@gmail.com>
2022-07-22 00:46:43 +0700

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact()],
+  site: 'https://limousine.netlify.app',
+  integrations: [sitemap()],
 });
